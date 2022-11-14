e6b88e04ffc715a55a853a6ee80163bcdf27118a
huandri <huandri@student.ethz.ch>
2022-11-14 15:19:25 +0100

    hello lello

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour {
-    public float movementSpeed = 1f;
+    private bool pressed = false;
     
     // Start is called before the first frame update
     void Start() {
@@ -13,12 +14,34 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
-            float horizontalMovement = Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime;
-            float verticalMovement = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+            float horizontalMovement = 0f;
+            float verticalMovement = 0f; 
+
+            if (!pressed) {
+                pressed = true;
+                if (Input.GetAxisRaw("Horizontal") != 0) {
+                    horizontalMovement = 1f * check(Input.GetAxisRaw("Horizontal"));
+                } else {
+                    verticalMovement = 1f * check(Input.GetAxisRaw("Vertical"));
+                }
+            }
 
             Vector3 movementDirection = new Vector3(horizontalMovement, verticalMovement, 0);
 
             gameObject.transform.Translate(movementDirection);
+        } else {
+            pressed = false;
         }
+    }
+
+    public static float check(float compare) {
+        if (compare == 0f)
+            return 0f;
+              
+        else if (compare < 0f)
+            return -1f;
+              
+        else
+            return 1f;
     }
 }
