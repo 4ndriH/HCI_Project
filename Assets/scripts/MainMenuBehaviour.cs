eba31aec8f2b5bbdaeadd8f068f902a8aff98552
k783s4 <chipleo.codes@gmail.com>
2022-12-01 16:46:25 +0100

    dfghjkdfghjfhjcxccghjcvh

--- Assets/scripts/MainMenuBehaviour.cs
+++ Assets/scripts/MainMenuBehaviour.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuBehaviour : MonoBehaviour
 {
+    public GameObject nameInput;
+
     public void LoadInstant(){
         Config.setInstantFeedback(true);
         Config.resetLevelNr();
+        Debug.Log(nameInput.GetComponent<TMP_InputField>().text);
+        Config.setName(nameInput.GetComponent<TMP_InputField>().text);
         SceneManager.LoadScene("LevelPrototype");
     }
      public void LoadAfter(){
         Config.setInstantFeedback(false);
         Config.resetLevelNr();
+        Config.setName(nameInput.GetComponent<TMP_InputField>().text);
         SceneManager.LoadScene("LevelPrototype");
     }
 }
